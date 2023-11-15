01357620a3845ac934a499b39ee764cfabb72195
LuisZentenxx <luis.zenteno02@inacapmail.cl>
2023-11-15 16:27:53 -0300

    feature(add style about section)

--- portafolio_react/src/components/About/About.jsx
+++ portafolio_react/src/components/About/About.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { getImageUrl } from "../../utils"
+import styles from "./About.module.css"
 
 export const About = () => {
-    return <section>
-        
+    return (
+    <section className={styles.container}>
+        <h2 className={styles.title}>Acerca de Mi</h2>
+        <div className={styles.content}>
+            <img
+                src={getImageUrl("me/me2.png")}
+                className={styles.aboutImg}
+            />
+            <ul className={styles.aboutItems}>
+                <li className={styles.aboutItem}>
+                    <img
+                        src={getImageUrl("about/cursor.png")}
+                        alt="cursor icon"
+                    />
+                    <div className={styles.aboutItemText}>
+                        <h3>Frontend Developer</h3>
+                        <p>
+                            Soy un desarrollador frontend que está aprendiendo React y Javascript
+                        </p>
+                    </div>
+                </li>
+                <li className={styles.aboutItem}>
+                    <img
+                        src={getImageUrl("about/backend.png")}
+                        alt="cursor icon"
+                    />
+                    <div className={styles.aboutItemText}>
+                        <h3>Backend Developer</h3>
+                        <p>Soy un desarrollador</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
     </section>
+    )
 };
--- portafolio_react/src/components/Me/Me.jsx
+++ portafolio_react/src/components/Me/Me.jsx
@@ -7,7 +7,8 @@ export const Me = () => {
     return <section className={styles.container}>
         <div className={styles.content}>
             <h1 className={styles.title}>Hola, soy Luis</h1>
-            <p className={styles.description}>Estudiante de programación</p>
+            <p className={styles.description}>Estudiante de programación akjsakdjaskdkasdkasajdaskdj
+            askdjaksdjaksdjaksdkajsdkasjd aksdjkadsjksdj kajdskasjdkasdj</p>
             <a href="mailto:fernandolzm98@gmail.com" className={styles.contactBtn}>Contáctame</a>
         </div>
         <img src={getImageUrl("me/me.png")} alt="My image" className={styles.meImg}/>
